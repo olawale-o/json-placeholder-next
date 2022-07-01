f7f242dfb1d85b371bb9862163fe5c9462b30e1c
olawale-o <omoogunolawale@gmail.com>
2022-07-01 13:27:43 +0100

    Use Nav component

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Nav from './Nav';
 
 const Header = () => {
   return (
-    <div>Header works</div>
+    <header className='header'>
+      <Nav />
+    </header>
   )
 }
 
